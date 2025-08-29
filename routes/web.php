7da70248e3393d3d067860dc73d58f0dd59b724c
sachuIsCool <sachinkumarv007@gmail.com>
2025-08-29 17:35:39 +0530

    chore: bring in Laravel 11 skeleton

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RestaurantController;
 
-Route::get('/', [RestaurantController::class, 'index'])->name('restaurants.index');
-Route::post('/search', [RestaurantController::class, 'search'])->name('restaurants.search');
-
-
+Route::get('/', function () {
+    return view('welcome');
+});
